437a8aafd0356e7b0fc245cfe03129985f405f96
andykarl43 <mboningjokungandykarl43@gmail.com>
2024-10-11 21:25:06 +0200

    connection ok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production:true,
+  production: true,
   // apiUrl:"http://localhost:3000/"
 
-   apiUrl:"https://app.closercm.net/public/"
+  apiUrl: "https://preprod.closercm.net/api/"
 
 };
